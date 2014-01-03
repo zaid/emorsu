6dfd87c923de05229bf1372f0629dabd69b5f8ed
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2014-01-03 00:11:03 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emorsu::Application.config.secret_key_base = 'a029591fd2729b6e8599fe05d7c55740463765e9bfcbe3153679f7c7d470d3bc14c32b4335766b34ba2530365ed23d01ad20f9cd0495a726da04b0331ccb3a29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emorsu::Application.config.session_store :cookie_store, key: '_emorsu_session'
