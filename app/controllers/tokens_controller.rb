560f543b65a7819e0fde1988cb64f8f04d2f6ff5
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2014-01-03 22:11:55 -0500

    Add support for parsing Morse code tokens

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,12 +1,19 @@
 class TokensController < ApplicationController
 
   def create
-    render json: { token: Morsu::Parser.parse_plaintext(token_params[:token] || '') }
+    parsed_token = Morsu::Parser.parse_plaintext(token_params[:token]) if is_plaintext?
+    parsed_token ||= Morsu::Parser.parse_morse_code(token_params[:token])
+
+    render json: { token: parsed_token }
   end
 
   private
 
   def token_params
     params.permit(:token)
+  end
+
+  def is_plaintext?
+    params[:token] =~ /\w+/
   end
 end
--- spec/controllers/tokens_controller_spec.rb
+++ spec/controllers/tokens_controller_spec.rb
@@ -10,19 +10,29 @@ describe TokensController do
       expect(response).to be_success
     end
 
+    it "should return an empty JSON string if nothing is passed in" do
+      post :create, token: nil
+
+      expect(data['token']).to be_blank
+    end
+
     context "plaintext" do
       let(:plaintext_token) { "Hello World" }
-
-      it "should return an empty JSON string if nothing is passed in" do
-        post :create, token: nil
-
-        expect(data['token']).to be_blank
-      end
 
       it "should convert plaintext to Morse code" do
         post :create, token: plaintext_token
 
         expect(data['token']).to eq(Morsu::Parser.parse_plaintext(plaintext_token))
+      end
+    end
+
+    context "morse code" do
+      let(:morse_code) { '... --- ...' }
+
+      it "should convert Morse code to plaintext" do
+        post :create, token: morse_code
+
+        expect(data['token']).to eq(Morsu::Parser.parse_morse_code(morse_code))
       end
     end
   end
